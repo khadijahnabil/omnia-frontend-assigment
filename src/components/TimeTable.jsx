844459f0b2409452d6c0670fdf363ef196cd27e7
khadijahnabil <nabilkhadijah7@gmail.com>
2024-02-11 16:28:01 +0100

    added time selection functionality

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -3,56 +3,21 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { useState, useEffect } from "react";
 
 const timeFrames = [
-  {
-    end_time: "09:00",
-    start_time: "08:00",
-  },
-  {
-    end_time: "10:00",
-    start_time: "09:00",
-  },
-  {
-    end_time: "11:00",
-    start_time: "10:00",
-  },
-  {
-    end_time: "12:00",
-    start_time: "11:00",
-  },
-  {
-    end_time: "13:00",
-    start_time: "12:00",
-  },
-  {
-    end_time: "14:00",
-    start_time: "13:00",
-  },
-  {
-    end_time: "15:00",
-    start_time: "14:00",
-  },
-  {
-    end_time: "16:00",
-    start_time: "15:00",
-  },
-  {
-    end_time: "17:00",
-    start_time: "16:00",
-  },
-  {
-    end_time: "18:00",
-    start_time: "17:00",
-  },
-  {
-    end_time: "19:00",
-    start_time: "18:00",
-  },
-  {
-    end_time: "20:00",
-    start_time: "19:00",
-  },
+  { id: 1, end_time: "09:00", start_time: "08:00" },
+  { id: 2, end_time: "10:00", start_time: "09:00" },
+  { id: 3, end_time: "11:00", start_time: "10:00" },
+  { id: 4, end_time: "12:00", start_time: "11:00" },
+  { id: 5, end_time: "13:00", start_time: "12:00" },
+  { id: 6, end_time: "14:00", start_time: "13:00" },
+  { id: 7, end_time: "15:00", start_time: "14:00" },
+  { id: 8, end_time: "16:00", start_time: "15:00" },
+  { id: 9, end_time: "17:00", start_time: "16:00" },
+  { id: 10, end_time: "18:00", start_time: "17:00" },
+  { id: 11, end_time: "19:00", start_time: "18:00" },
+  { id: 12, end_time: "20:00", start_time: "19:00" },
 ];
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -62,12 +27,13 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const handleTimeSelection = () => {
-  console.log("picked time");
-};
-
 const TimeTable = ({ availability, selectedDate }) => {
+  const [buttonsPressed, setButtonsPressed] = useState([]);
   const formattedDate = selectedDate.format("YYYY-MM-DD");
+
+  useEffect(() => {
+    setButtonsPressed([]);
+  }, [selectedDate]);
 
   const getTimeAvailability = (availabilityData, formattedDate) => {
     return availabilityData.find(
@@ -122,25 +88,46 @@ const TimeTable = ({ availability, selectedDate }) => {
 
   const hourlyAvRanges = toHourlyAvailabilityRanges(availableHours);
 
+  const handleTimeSelection = (range) => {
+    setButtonsPressed((prevState) => {
+      if (prevState.map((x) => x.id).includes(range.id)) {
+        return [...prevState.filter((state) => state.id !== range.id)];
+      }
+      if (prevState.length >= 2) {
+        console.log("cannot add more time, maximum 2");
+        return [...prevState];
+      }
+      if (range.id > prevState[0]?.id + 1 || range.id < prevState[0]?.id - 1) {
+        console.log("please select 2 max time slots next to each other");
+        return [...prevState];
+      }
+      return [...prevState, range];
+    });
+  };
+
   return (
     <Box className='mt-2.5'>
       <Grid container spacing={2}>
-        {timeFrames.map((range, index) => {
+        {timeFrames.map((range) => {
           return (
-            <Grid item xs={4} key={index}>
-              <Item>
-                <Button
-                  onClick={handleTimeSelection}
-                  disabled={
-                    !hourlyAvRanges.some(
-                      (r) =>
-                        r.start_time === range.start_time &&
-                        r.end_time === range.end_time
-                    )
-                  }>
-                  {range.start_time} - {range.end_time}
-                </Button>
-              </Item>
+            <Grid item xs={4} key={range.id}>
+              <Button
+                variant={
+                  buttonsPressed[0]?.id === range.id ||
+                  buttonsPressed[1]?.id === range.id
+                    ? "contained"
+                    : "outlined"
+                }
+                onClick={() => handleTimeSelection(range)}
+                disabled={
+                  !hourlyAvRanges.some(
+                    (r) =>
+                      r.start_time === range.start_time &&
+                      r.end_time === range.end_time
+                  )
+                }>
+                {range.start_time} - {range.end_time}
+              </Button>
             </Grid>
           );
         })}
